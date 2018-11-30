ba236c63aef020f0fc25d589abcd1ea4b393a965
github username <ariellenramirez@gmail.com>
2018-11-30 14:45:07 -0500

    added profile pic, info under pic and started vertical line

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,13 +1,24 @@
 import React, { Component, Fragment } from "react";
 import background from "../images/background.png";
 import NavBar from "../Components/NavBar";
+import me from "../images/me.png";
 
 class Resume extends Component {
   render() {
     return (
       <Fragment>
         <div className="resumebox">
-          <div />
+          <div>
+            <img className="profile" src={me} />
+            <article>
+              <h1>Arielle Ramirez</h1>
+              <p>Email: ariellenramirez@gmail.com</p>
+              <p>Phone: 603-721-6388</p>
+              <p>Linkedin:</p>
+              <p>Git:</p>
+            </article>
+            <nav className="vline"> </nav>
+          </div>
         </div>
       </Fragment>
     );
