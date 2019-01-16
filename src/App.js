c409bd9b54415de3ddf88df1cffe96cec87d2883
Arielle Ramirez <ariellenramirez@gmail.com>
2019-01-15 22:49:30 -0500

    added and adjusted routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from "react";
 import Contact from "./Components/Contact";
 import HomePage from "./Components/HomePage";
 import Resume from "./Components/Resume";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/resume" component={Resume} />
-          <Route exact path="/contact" component={Contact} />
-        </div>
-      </Router>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/resume" component={Resume} exact />
+          <Route path="/projects" component={Projects} exact />
+          <Route path="/contact" component={Contact} exact />
+
+          <Route component={Error} exact />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
