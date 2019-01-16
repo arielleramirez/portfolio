25cd3ef52e86917b7605a26adc06d584f02b1af0
Arielle Ramirez <ariellenramirez@gmail.com>
2019-01-15 22:34:52 -0500

    readjusted homepage

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,30 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class Contact extends Component {
+  handleClick = event => {
+    console.log(event.target);
+  };
   render() {
-    return <div>Contact</div>;
+    return (
+      <Fragment>
+        <div className="body flex-container">
+          <div className="box1">
+            <h1>Arielle Ramirez</h1>
+            <h3>Full Stack Developer/Software Engineer</h3>
+          </div>
+          <div onClick={this.handleClick}>
+            <div className="container arrow" onClick={this.handleClick}>
+              <div className="triangle1" />
+            </div>
+          </div>
+          <div>
+            <div className="container arrow" onClick={this.handleClick}>
+              <div className="triangle2" />
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
