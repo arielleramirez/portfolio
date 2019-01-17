ef7e73223e9ad179fccf39d2868445ed624af941
Arielle Ramirez <ariellenramirez@gmail.com>
2019-01-17 16:53:41 -0500

    added arrows to projects page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,21 +7,8 @@ class Contact extends Component {
   render() {
     return (
       <Fragment>
-        <div className="body flex-container">
-          <div className="box1">
-            <h1>Arielle Ramirez</h1>
-            <h3>Full Stack Developer/Software Engineer</h3>
-          </div>
-          <div onClick={this.handleClick}>
-            <div className="container arrow" onClick={this.handleClick}>
-              <div className="triangle1" />
-            </div>
-          </div>
-          <div>
-            <div className="container arrow" onClick={this.handleClick}>
-              <div className="triangle2" />
-            </div>
-          </div>
+        <div>
+          <h2>Contact Page</h2>
         </div>
       </Fragment>
     );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,7 +4,23 @@ class Projects extends Component {
   render() {
     return (
       <div>
-        <h2>Projects Page</h2>
+        <div className="box3">
+          <div>
+            <h1>Projects</h1>
+          </div>
+        </div>
+        <div className="box4">
+          <div onClick={() => this.props.history.push("/contact")}>
+            <div className="container arrow1">
+              <div className="triangle3" />
+            </div>
+            <div>
+              <div className="container arrow2">
+                <div className="triangle4" />
+              </div>
+            </div>
+          </div>
+        </div>
         <div class="flex-box">
           <div>1</div>
           <div>2</div>
