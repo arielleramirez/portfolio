4397f9b4da06b62527d62b7eab25bd6b4e7da501
Arielle Ramirez <ariellenramirez@gmail.com>
2019-01-17 17:05:40 -0500

    adjusting styling

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,6 +4,25 @@ class Projects extends Component {
   render() {
     return (
       <div>
+        <div className="flex-container">
+          <div className="box3">
+            <div>
+              <h1>Projects</h1>
+            </div>
+          </div>
+          <div className="box4">
+            <div onClick={() => this.props.history.push("/projects")}>
+              <div className="container arrow">
+                <div className="triangle1" />
+              </div>
+              <div>
+                <div className="container arrow">
+                  <div className="triangle2" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="box3">
           <div>
             <h1>Projects</h1>
