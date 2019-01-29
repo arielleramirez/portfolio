3ca8028bcca3c6d49c382fbe5a8c0a00134e9c58
Arielle Ramirez <ariellenramirez@gmail.com>
2019-01-29 11:57:13 -0500

    added images and background color to projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import parkit from "../images/parkitimg.png";
+import guerra from "../images/guerra.png";
+import rogue from "../images/rogueimg.png";
 
 class Projects extends Component {
   render() {
@@ -25,14 +28,15 @@ class Projects extends Component {
         </div>
 
         <div class="flex-box">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
+          <div>
+            <img src={parkit} />
+          </div>
+          <div>
+            <img src={guerra} />
+          </div>
+          <div>
+            <img src={rogue} />
+          </div>
         </div>
       </div>
     );
